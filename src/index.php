9a92d14991394257b4f69e2d36bfe2ea4b868e45
Ali Rizwan <arizwan@alphabold.com>
2019-10-28 13:16:50 +0500

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>My Shop</title>
+    </head>
+
+    <body>
+        <h1>Welcome to my shop</h1>
+        <p1>
+	consider it Yours...well, I'm kidding... but still Enjoy.
+	</p1>
+    </body>
+</html>
