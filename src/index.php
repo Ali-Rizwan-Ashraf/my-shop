17f6f9df6877b0aa3290af438c363ead84ec59d3
Ali Rizwan <arizwan@alphabold.com>
2019-11-11 17:04:36 +0500

    html text increased

--- src/index.php
+++ src/index.php
@@ -4,13 +4,13 @@
     </head>
 
     <body>
-        <h1>Welcome to my shop</h1>
+        <h1>Welcome, Welcome, Welcome...Welcome to my shop</h1>
         <p1>
 	        Consider it Yours...well, I'm kidding... but still Enjoy.
             <br> So,wanna buy something???
             <br> NO?
             <br> Then get Lost.
-            <br> Haha... I'm just kidding
+            <br> Haha... I'm Just kidding
 	        <br> or Maybe Not
 	        <br>
 	        <br>
